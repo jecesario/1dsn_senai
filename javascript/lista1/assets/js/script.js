544e965d9cb8fd7f6d368032c7da4a7e65b6693c
Jecesario <jp@soloid.com.br>
2021-03-01 12:37:23 -0300

    feat(areaCirculo): adicionando funcionalidade de calcular area do circulo

--- javascript/lista1/assets/js/script.js
+++ javascript/lista1/assets/js/script.js
@@ -10,3 +10,12 @@ soma();
 function apagar() {
     document.write("imagem apagada");
 }
+
+// Script do Exercício 3
+function areaCirculo() {
+    var raio = 10;
+    var resultado = 3.14 * (raio ** 2);
+    window.alert("Área do circulo: " + resultado);
+}
+
+areaCirculo();
